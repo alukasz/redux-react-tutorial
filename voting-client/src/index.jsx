1eebd5ffab7cfb67e383afa50050e0eafe6a304a
Łukasz Antończyk <antonczyklukasz@gmail.com>
2015-11-01 22:45:12 +0100

    Add store to index.jsx

--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -3,9 +3,22 @@ require('./style.css');
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Router, {Route} from 'react-router';
+import {createStore} from 'redux';
+import reducer from './reducer';
 import App from './components/App';
 import Voting from './components/Voting';
 import Results from './components/Results';
+
+const store = createStore(reducer);
+store.dispatch({
+  type: 'SET_STATE',
+  state: {
+    vote: {
+      pair: ['Sunshine', '28 Days Later'],
+      tally: {Sunshine: 2}
+    }
+  }
+});
 
 const routes = <Route component={App}>
   <Route path="/results" component={Results} />
