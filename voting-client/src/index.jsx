ac97a75196da9ce91f236eb850d6446e54b83061
Łukasz Antończyk <antonczyklukasz@gmail.com>
2015-11-01 16:28:13 +0100

    Add Voting component

--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -7,6 +7,6 @@ import Voting from './components/Voting';
 const pair = ['Trainspotting', '28 Days Later'];
 
 ReactDOM.render(
-  <Voting pair={pair} />
+  <Voting pair={pair} />,
   document.getElementById('app')
 );
