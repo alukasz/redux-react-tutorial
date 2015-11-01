8f2850ed581245e620733bff0dc99bcf38bf800d
Łukasz Antończyk <antonczyklukasz@gmail.com>
2015-11-01 16:17:21 +0100

    Improve index.jsx to render Voting component

---
+++ voting-client/src/index.jsx
@@ -0,0 +1,12 @@
+require('./style.css');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Voting from './components/Voting';
+
+const pair = ['Trainspotting', '28 Days Later'];
+
+ReactDOM.render(
+  <Voting pair={pair} />
+  document.getElementById('app')
+);
--- voting-client/webpack.config.js
+++ voting-client/webpack.config.js
@@ -4,15 +4,18 @@ module.exports = {
   entry: [
     'webpack-dev-server/client?http://localhost:8080',
     'webpack/hot/only-dev-server',
-    './src/index.js'
+    './src/index.jsx'
   ],
   module: {
     loaders: [{
       test: /\.jsx?$/,
       exclude: /node_modules/,
       loader: 'react-hot!babel'
+    }, {
+      test: /\.css$/,
+      loader: 'style!css!autoprefixer?browsers=last 2 versions'
     }],
-  }
+  },
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
