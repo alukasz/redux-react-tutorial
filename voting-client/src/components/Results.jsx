808b3b6df06bbb5d07fadb33f80dfe6d72fe9e8a
Łukasz Antończyk <antonczyklukasz@gmail.com>
2015-11-01 21:56:59 +0100

    Finish Results component and tests

--- voting-client/src/components/Results.jsx
+++ voting-client/src/components/Results.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
+import Winner from './Winner';
 
 export default React.createClass({
   mixins: [PureRenderMixin],
@@ -21,22 +22,31 @@ export default React.createClass({
     return ((this.getVotes(entry) / this.sumOfVotes()) * 100) + '%';
   },
   render: function() {
-    return <div className="results">
-      <div className="tally">
-        {this.getPair().map(entry =>
-          <div key={entry} className="entry">
-            <h1>{ entry }</h1>
-            <div className="voteVisualization">
-              <div className="votesBlock"
-                   style={{width: this.getVotesBlockWidth(entry)}}>
+    return this.props.winner ?
+      <Winner ref="winner" winner={this.props.winner} /> :
+      <div className="results">
+        <div className="tally">
+          {this.getPair().map(entry =>
+            <div key={entry} className="entry">
+              <h1>{ entry }</h1>
+              <div className="voteVisualization">
+                <div className="votesBlock"
+                     style={{width: this.getVotesBlockWidth(entry)}}>
+                </div>
+              </div>
+              <div className="voteCount">
+                {this.getVotes(entry)}
               </div>
             </div>
-            <div className="voteCount">
-              {this.getVotes(entry)}
-            </div>
-          </div>
-        )}
-      </div>
-    </div>;
+          )}
+        </div>
+        <div className="management">
+          <button ref="next"
+                  className="next"
+                  onClick={this.props.next}>
+            Next
+          </button>
+        </div>
+      </div>;
   }
 });
