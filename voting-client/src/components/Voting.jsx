f66cab76aca6f5459043c0dc9e305417f46243c7
Łukasz Antończyk <antonczyklukasz@gmail.com>
2015-11-01 18:02:17 +0100

    Split Voting component into Voting, Vote and Winner

--- voting-client/src/components/Voting.jsx
+++ voting-client/src/components/Voting.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
+import Winner from './Winner';
+import Vote from './Vote';
 
 export default React.createClass({
   getPair: function() {
     return this.props.pair || [];
   },
+  isDisabled: function() {
+    return !!this.props.hasVoted;
+  },
+  hasVotedFor: function(entry) {
+    return this.props.hasVoted === entry;
+  },
   render: function() {
-    return <div className="voting">
-      {this.getPair().map(entry =>
-        <button key={ entry }>
-          <h1>{ entry }</h1>
-        </button>
-      )}
+    return <div>
+      { this.props.winner ?
+        <Winner ref="winner" winner={ this.props.winner }/> :
+        <Vote { ...this.props } />
+      }
     </div>;
   }
 });
