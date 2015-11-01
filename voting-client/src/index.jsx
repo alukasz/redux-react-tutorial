f81db2a6e4abbbc37228cd6a91bc0998a3ad61db
Łukasz Antończyk <antonczyklukasz@gmail.com>
2015-11-01 20:16:00 +0100

    Add React Router

--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -2,11 +2,15 @@ require('./style.css');
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Router, {Route} from 'react-router';
+import App from './components/App';
 import Voting from './components/Voting';
 
-const pair = ['Trainspotting', '28 Days Later'];
+const routes = <Route component={App}>
+  <Route path="/" component={Voting} />
+</Route>;
 
 ReactDOM.render(
-  <Voting pair={pair}/>,
+  <Router>{routes}</Router>,
   document.getElementById('app')
 );
